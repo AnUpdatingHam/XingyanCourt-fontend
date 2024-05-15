0e805f6c020062ca729aff9158defce1b921e88e
ham <1319716110@qq.com>
2024-05-15 20:30:09 +0800

    基础模板工程，包含Aside、Header、路由

--- vue-QuizeWise/src/main.js
+++ vue-QuizeWise/src/main.js
@@ -1,6 +1,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+// element-ui plus 的引入
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import router from './router'
+// icon 图标引入
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// icon 图标挂载
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(createPinia())
+app.use(router)
+// 挂载到app实例上
+app.use(ElementPlus)
+app.mount('#app')
---
+++ vue-QuizeWise/src/views/NoFoundVue.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <i> 404 No Found </i>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  div{
+    display: flex;
+    width: 1500px;
+    height: 600px;
+    align-items: center;
+    justify-content: center;
+  }
+  i {
+    display: flex;
+    place-items: center;
+    place-content: center;
+    width: 500px;
+    height: 300px;
+    font-size: 50px;
+    color: var(--color-text);
+  }
+</style>
