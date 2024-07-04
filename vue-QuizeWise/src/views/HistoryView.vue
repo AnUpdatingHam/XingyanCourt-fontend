3a7fc5a0c12656558b0a428825536b0b838e1192
凌金涛 <2596256623@qq.com>
2024-07-04 23:17:21 +0800

    修改了HistoryView

--- vue-QuizeWise/src/views/HistoryView.vue
+++ vue-QuizeWise/src/views/HistoryView.vue
@@ -28,15 +28,15 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(item,index) in displayData" :key="item.index" @click="goToPaper">
+      <tr v-for="(item,index) in displayData" :key="item.id" @click="goToPaper">
         <td>{{ item.name }}</td>
         <td>{{ item.subject }}</td>
         <td>{{ item.grade }}</td>
-        <td>{{ item.exam }}</td>
-        <td>{{ item.pageNumber }}</td>
-        <td>{{ item.problemNumber }}</td>
-        <td>{{ item.answer }}</td>
-        <td>{{ item.time }}</td>
+        <td>{{ item.type }}</td>
+        <td>{{ item.page_count }}</td>
+        <td>{{ item.question_count }}</td>
+        <td>{{ item.contain_answer }}</td>
+        <td>{{ item.create_time }}</td>
       </tr>
     </tbody>
   </table>
@@ -47,7 +47,7 @@
     :page-sizes="[10]"
     v-model:page-size="state.pageSize" 
     layout="total, sizes, prev, pager, next, jumper" 
-    :total="state.totalNumber"
+    :total="state.total"
     @size-change="handleSizeChange" 
     @current-change="handleCurrentChange" 
     style="float:right">
@@ -59,52 +59,38 @@
 // 引入 Axios 库
 import axios from 'axios';
 
-// 定义一个异步函数来发送 GET 请求
-async function getUser() {
-  try {
-    // 使用 Axios 发送 GET 请求
-    //测试获取id为1的用户数据
-    const response = await axios.get('http://localhost:8080/user/user/1');
-    // 请求成功，'response' 包含了请求的结果
-    console.log(response.data); // 打印请求返回的数据
-  } catch (error) {
-    // 请求失败，捕获并处理错误
-    console.error('Error fetching data:', error);
-  }
-}
-//getUser();
+// // 定义要发送的数据
+// const userData = {
+//   id: '1',
+//   username: 'newUserName',
+//   phone: '123456789',
+//   email: 'newEmail'
+//   // 其他用户属性...
+// };
 
-// 定义要发送的数据
-const userData = {
-  id: '1',
-  username: 'newUserName',
-  phone: '123456789',
-  email: 'newEmail'
-  // 其他用户属性...
-};
-
-// 定义一个异步函数来发送 PUT 请求，更改对应id的用户信息
-async function updateUser() {
-  try {
-    // 使用 Axios 发送 POST 请求，并包含 JSON 数据
-    const response = await axios.put('http://localhost:8080/user/user', userData, {
-      // 设置请求头，指明内容类型为 JSON
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    // 请求成功，打印响应数据
-    console.log(response.data);
-  } catch (error) {
-    // 请求失败，捕获并处理错误
-    console.error('Error update user:', error);
-  }
-}
-updateUser();
+// // 定义一个异步函数来发送 PUT 请求，更改对应id的用户信息
+// async function updateUser() {
+//   try {
+//     // 使用 Axios 发送 POST 请求，并包含 JSON 数据
+//     const response = await axios.put('http://localhost:8080/user/user', userData, {
+//       // 设置请求头，指明内容类型为 JSON
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+//     // 请求成功，打印响应数据
+//     console.log(response.data);
+//   } catch (error) {
+//     // 请求失败，捕获并处理错误
+//     console.error('Error update user:', error);
+//   }
+// }
+// updateUser();
 
 export default{
   data(){
     return{
+
       options: ["语文","数学","英语"],
       active: -1,
       search: '',
@@ -114,34 +100,13 @@ export default{
         createTimeEnd: "",
         createTimeStart: "",
         searchValue: "",
-        totalNumber: 24,    //n
+        total: 0,    //n
       },
-      historyData:[         //n组数据
-        { name: "未命名22", subject: '语文',  grade: '高三', exam: '高考', pageNumber: 3, problemNumber: 34, answer: '有', time: "2024.5.6"},
-        { name: "未命名21", subject: '英语',  grade: '初二', exam: '中考', pageNumber: 5, problemNumber: 44, answer: '无', time: "2024.5.1"},
-        { name: "未命名23", subject: '数学',  grade: '高一', exam: '高考', pageNumber: 3, problemNumber: 10, answer: '有', time: "2024.4.28"},
-        { name: "未命名24", subject: '语文',  grade: '初三', exam: '中考', pageNumber: 3, problemNumber: 30, answer: '有', time: "2024.4.28"},
-        { name: "未命名25", subject: '英语',  grade: '高一', exam: '高考', pageNumber: 5, problemNumber: 44, answer: '无', time: "2024.4.28"},
-        { name: "未命名26", subject: '数学',  grade: '高一', exam: '高考', pageNumber: 2, problemNumber: 12, answer: '有', time: "2024.4.21"},
-        { name: "未命名27", subject: '语文',  grade: '初三', exam: '中考', pageNumber: 3, problemNumber: 30, answer: '有', time: "2024.4.21"},
-        { name: "未命名28", subject: '英语',  grade: '初二', exam: '中考', pageNumber: 5, problemNumber: 50, answer: '无', time: "2024.4.21"},
-        { name: "未命名29", subject: '数学',  grade: '高一', exam: '高考', pageNumber: 2, problemNumber: 12, answer: '有', time: "2024.4.19"},
-        { name: "未命名20", subject: '语文',  grade: '初一', exam: '中考', pageNumber: 3, problemNumber: 28, answer: '有', time: "2024.4.19"},
-        { name: "未命名32", subject: '英语',  grade: '初二', exam: '中考', pageNumber: 5, problemNumber: 44, answer: '无', time: "2024.4.19"},
-        { name: "未命名34", subject: '数学',  grade: '高一', exam: '高考', pageNumber: 2, problemNumber: 14, answer: '有', time: "2024.4.16"},
-        { name: "未命名36", subject: '英语',  grade: '高二', exam: '高考', pageNumber: 3, problemNumber: 30, answer: '有', time: "2024.4.10"},
-        { name: "未命名11", subject: '英语',  grade: '初二', exam: '中考', pageNumber: 5, problemNumber: 45, answer: '有', time: "2024.4.10"},
-        { name: "未命名12", subject: '数学',  grade: '初二', exam: '中考', pageNumber: 2, problemNumber: 12, answer: '无', time: "2024.4.10"},
-        { name: "未命名13", subject: '语文',  grade: '初三', exam: '中考', pageNumber: 3, problemNumber: 30, answer: '有', time: "2024.4.10"},
-        { name: "未命名14", subject: '英语',  grade: '初二', exam: '中考', pageNumber: 5, problemNumber: 40, answer: '无', time: "2024.4.10"},
-        { name: "未命名15", subject: '数学',  grade: '初三', exam: '中考', pageNumber: 2, problemNumber: 12, answer: '有', time: "2024.4.10"},
-        { name: "未命名16", subject: '语文',  grade: '初三', exam: '中考', pageNumber: 3, problemNumber: 32, answer: '无', time: "2024.4.9"},
-        { name: "未命名17", subject: '英语',  grade: '初二', exam: '中考', pageNumber: 5, problemNumber: 44, answer: '有', time: "2024.4.7"},
-        { name: "未命名18", subject: '数学',  grade: '高一', exam: '高考', pageNumber: 2, problemNumber: 12, answer: '有', time: "2024.4.5"},
-        { name: "未命名19", subject: '语文',  grade: '初二', exam: '中考', pageNumber: 3, problemNumber: 29, answer: '有', time: "2024.4.2"},
-        { name: "未命名44", subject: '英语',  grade: '初二', exam: '中考', pageNumber: 5, problemNumber: 44, answer: '无', time: "2024.4.1"},
-        { name: "未命名43", subject: '数学',  grade: '高二', exam: '高考', pageNumber: 2, problemNumber: 10, answer: '无', time: "2024.4.1"},
-      ],
+      historyData:[],
+        // { id: 1, user_id: "0", name: "未命名22", subject: '语文',  grade: '高三', type: '高考', page_count: 3, question_count: 34, contain_answer: '有', path: "rrr", create_time: "2024.5.6"},
+        // { id: 2, user_id: "0", name: "未命名21", subject: '英语',  grade: '初二', type: '中考', page_count: 5, question_count: 44, contain_answer: '无', path: "www", create_time: "2024.5.1"},
+        // { id: 3, user_id: "0", name: "未命名23", subject: '数学',  grade: '高一', type: '高考', page_count: 3, question_count: 10, contain_answer: '有', path: "uuu", create_time: "2024.4.28"},
+        
     }
   },
   methods:{
@@ -159,43 +124,64 @@ export default{
     },
     switchActive2(){
       this.active=-1
-    }
+    },
+    // 定义一个异步函数来发送 GET 请求
+    async getUser() {
+      try {
+        // 使用 Axios 发送 GET 请求
+        //测试获取id为1的用户数据
+        const response = await axios.get('http://localhost:8080/user/user/1');
+        // 请求成功，'response' 包含了请求的结果
+        console.log(response.data); // 打印请求返回的数据
+        this.historyData=response.data.data.records
+        this.total=response.data.data.total
+
+      } catch (error) {
+        // 请求失败，捕获并处理错误
+        console.error('Error fetching data:', error);
+      }
+    },
   },
   computed:{
     displayData(){
-      let data
+      return this.historyData
 
-      if(this.active>-1){
-        let subjectData=[];
-        for(let i=0;i<this.historyData.length;i++)
-          if(this.options[this.active] === this.historyData[i].subject)
-            subjectData.push(this.historyData[i])
-        data=subjectData
-      }
-      else
-        data=this.historyData
+      // let data
 
-      if(this.search){
-        let searchData=[]
-        for(let i=0;i<data.length;i++)
-          for(let key in data[i]){
-            let it
-            if(typeof data[i][key] === "number")
-              it=String(data[i][key])
-            else
-              it=data[i][key]
+      // if(this.active>-1){
+      //   let subjectData=[];
+      //   for(let i=0;i<this.historyData.length;i++)
+      //     if(this.options[this.active] === this.historyData[i].subject)
+      //       subjectData.push(this.historyData[i])
+      //   data=subjectData
+      // }
+      // else
+      //   data=this.historyData
 
-            if(it.includes(this.search)){
-              searchData.push(data[i])
-              break
-            }
-          }
+      // if(this.search){
+      //   let searchData=[]
+      //   for(let i=0;i<data.length;i++)
+      //     for(let key in data[i]){
+      //       let it
+      //       if(typeof data[i][key] === "number")
+      //         it=String(data[i][key])
+      //       else
+      //         it=data[i][key]
+
+      //       if(it.includes(this.search)){
+      //         searchData.push(data[i])
+      //         break
+      //       }
+      //     }
             
-        data=searchData
-      }
+      //   data=searchData
+      // }
 
-      return data.slice((this.state.pageNum-1)*this.state.pageSize , this.state.pageNum*this.state.pageSize)
+      // return data.slice((this.state.pageNum-1)*this.state.pageSize , this.state.pageNum*this.state.pageSize)
     }
+  },
+  mounted(){
+    this.getUser()
   }
 }
 </script>
